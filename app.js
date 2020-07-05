4e5ecc0c98edc53d3fc1194254a6484aa3a7501a
Bryant Son <kaita.contact@gmail.com>
2020-07-04 22:18:12 -0500

    Changing router

--- app.js
+++ app.js
@@ -39,13 +39,12 @@ con.connect(function(err) {
 });
 */
 
-app.use("/", router.get("/", function(req, res, next) {
+app.get("/", function(req, res, next) {
     res.locals.connection.query("SELECT * FROM pets", function(error, result, fields) {
       if(error) throw error;
       res.send(JSON.stringify(result));
     });
-  });
-);
+});
 
 var http = require("http");
 var server = http.createServer(app);
