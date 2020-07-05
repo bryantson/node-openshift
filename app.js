633701c13b8c775b6ff1aecfe8178e25e25119c1
Bryant Son <kaita.contact@gmail.com>
2020-07-04 22:07:07 -0500

    making MYSQL call

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var express = require("express");
+var router = express.Router();
+
 var mysql = require("mysql");
 
 const MYSQL_SERVICE_HOST = process.env.MYSQL_SERVICE_HOST;
@@ -6,6 +9,21 @@ const MYSQL_USER = process.env.MYSQL_USER;
 const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE;
 
+var app = express();
+
+app.use(function(req, res, next) {
+  res.locals.connection = mysql.createConnection({
+    host: MYSQL_SERVICE_HOST,
+    port: MYSQL_SERVICE_PORT,
+    database: MYSQL_DATABASE,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD
+  });
+  res.locals.connection.connect();
+  next();
+});
+
+/*
 var con = mysql.createConnection({
   host: MYSQL_SERVICE_HOST,
   port: MYSQL_SERVICE_PORT,
@@ -14,7 +32,16 @@ var con = mysql.createConnection({
   password: MYSQL_PASSWORD
 });
 
+
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected");
+});
+*/
+
+router.get("/", function(req, res, next) {
+  res.locals.connection.query("SELECT * FROM pets", function(error, result, fields) {
+    if(error) throw error;
+    res.send(JSON.stringify(result));
+  });
 });
