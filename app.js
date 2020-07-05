7fd9c66da58f231f4a56500b237d2394c1c3a852
Bryant Son <kaita.contact@gmail.com>
2020-07-04 21:55:01 -0500

    Changed the variable to normal

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE;
 
 var con = mysql.createConnection({
-  host: ${MYSQL_SERVICE_HOST},
-  port: ${MYSQL_SERVICE_PORT},
-  database: ${MYSQL_DATABASE},
-  user: ${MYSQL_USER},
-  password: ${MYSQL_PASSWORD}
+  host: MYSQL_SERVICE_HOST,
+  port: MYSQL_SERVICE_PORT,
+  database: MYSQL_DATABASE,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD
 });
 
 con.connect(function(err) {
