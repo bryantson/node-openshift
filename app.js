d4b15b2286d7857babbb8c017949c036a93fcbe5
Bryant Son <kaita.contact@gmail.com>
2020-07-04 21:42:01 -0500

    Adding with mysql

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var mysql = require("mysql");
 
+const MYSQL_SERVICE_HOST = process.env.MYSQL_SERVICE_HOST;
+
+console.log(`Your HOST is:  ${MYSQL_SERVICE_HOST}`);
+
 var con = mysql.createConnection({
   host: "mysql.mysql-react.svc.cluster.local",
   database: "dbtest",
